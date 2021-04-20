f77779b6a7b7d904aff7b60a9d298527e4596d20
047bF <50794098+047bF@users.noreply.github.com>
2021-04-20 17:24:30 +0300

    Update yoox.py

--- python_scripts/yoox.py
+++ python_scripts/yoox.py
@@ -1,96 +1,157 @@
 # -*- coding: utf-8 -*-
-import sys
+# import sys
 import requests
 from bs4 import BeautifulSoup
-import os
 import time
-from db_actions import insert_input
+import json
+from db_actions import insert_input, get_actual_models, insert_new_product, \
+	update_model_price, update_model_size, update_model_navidate
+# from traceback import print_exc
+from datetime import datetime, timedelta
+from loguru import logger
+import re
 
+
+logger.add('debug.log', format="{time} {level}: ({file}:{module} - {line}) >> {message}", level="DEBUG")
+logger.add('info.log', format="{time} {level}: ({file}:{module} - {line}) >> {message}", level="INFO")
 session = requests.session()
 session.trust_env = False
-headers = {
-		'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:45.0) Gecko/20100101 Firefox/45.0'
-	}
+headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:45.0) Gecko/20100101 Firefox/45.0'}
 
-def load_user_data(pagenum, session):
-	#формирование страницы
-	url = 'https://www.yoox.com/RU/shoponline?dept=shoesmen&gender=U&page='+ str(pagenum) + '&sort=3&clientabt=SmsMultiChannel_ON%2CSizeIsocode_ON%2CNewDelivery_ON%2CFindYourSize_ON%2CNewPayment_ON%2Cfitanalytics_OFF'
-	request = session.get(url).text
-	request = BeautifulSoup(request, 'html.parser')
-	#print(url)
-	return request
+
+def load_user_data(pagenum_in, session_in):
+	# формирование страницы
+	url = 'https://www.yoox.com/RU/shoponline?dept=shoesmen&gender=U&page=' + str(pagenum_in) + \
+		'&sort=3&clientabt=SmsMultiChannel_ON%2CSizeIsocode_ON%2CNewDelivery_ON%2CFindYourSize_ON%2CNewPayment_' \
+		'ON%2Cfitanalytics_OFF'
+	response = session_in.get(url)
+	# logger.info(
+	# 	str(datetime.now())
+	# 	+ ' "Request":"create_request_yoox"\n "Headers":'
+	# 	+ str(response.headers)
+	# 	+ '\n "Response":'
+	# 	+ str(response.status_code)
+	# )
+	result = BeautifulSoup(response.text, 'html.parser')
+	return result
+
 
 def next_page(text):
-	#проверка сопоставления текущей страницы и разрешённой последней
-	out = text.find('ul', {'class': 'pagination list-inline pull-right text-center js-pagination'}).find('li', {'class':'selected-bold'}).text
+	# проверка сопоставления текущей страницы и разрешённой последней
+	out = text.find('ul', {'class': 'pagination list-inline pull-right text-center js-pagination'})\
+		.find('li', {'class': 'selected-bold'}).text
+	#logger.debug(out)
 	return out
 
-def prod_grid (text):
-	#не используется
+
+def prod_grid(text):
+	# не используется
 	out = text.find('div', {'class': 'col-8-24'})
-	#print('Finded', out.find('div').text)
+	#logger.debug(out)
+	# print('Finded', out.find('div').text)
 	return out is not None
+
 
 def prod_not_none(text):
-	#проверка, не продан ли продукт на странице
-	out = text.find('a', {'class':'itemlink'}).find('span')
+	# проверка, не продан ли продукт на странице
+	# out = text.find('a')['href']
+	out = text.find('a', {'class': 'itemlink'}).get('href')
+	#logger.debug(out)
 	return out is not None
 
-os.remove('./yoox_prod_export.log')
+
+def list_add_to_dict(list_in):
+	for value in list_in:
+		print(value)
+
+
+params = '{"yoox_id":1}'
+data = json.loads(params)
+app_id = data["yoox_id"]
+# os.open('./params.txt','r') as parameters
+
 pagenum = 1
-links_exp = {}
-with open('./logs/yoox_error_export'+time.strftime("%Y%m%d-%H")+'.txt', 'a') as error_export: #вывод из else
-	with open('./yoox_prod_export.log', 'a') as export_file: #основой вывод всей инфы
-		while True:
-			checker = []
-			data = load_user_data(pagenum, session)
-			try:
-				if int(next_page(data)) == pagenum:
-					items = data.find('div', {'id': 'itemsGrid'})
-					so_far = items.findAll('div', {'class': 'col-8-24'})
-					#поиск на всей странице в инфе о продукте
-					for n in so_far:
-						export_line = '' #начинаем экспорт файл
-						#код продукта, используется для поиска в словаре
-						prod_id = (n('div')[0]['data-current-cod10']) 
-						if prod_not_none(n):
-							if prod_id not in links_exp:
-								#ссылка на продукт
-								links_exp[n('div')[0]['data-current-cod10']]=n('a')[0]['href'] 
-								#берём бренд
-								export_line += prod_id + '|' + n.find('div', {'class':'brand font-bold text-uppercase'}).text + '|'
-								if n.find('div', {'class':'title'}):
-									description = n.find('div', {'class':'title'}).text
-								else:
-									description = prod_id
-								export_line += description + '|' 
-								#выбаорка цены, если со скидкой, выдаётся обе цены
-								price = n.find('div', {'class':'price'}).findAll('span')  
-								if price[0]['class'][0] == 'fullprice':
-									export_line += price[0].text + ':fullprice|'
-								else:
-									export_line += price[0].text + ':' + price[1].text + '|'
-								# выборка размеров, в конце запятая
-								size = n.find('div', {'class':'size text-light'}).findAll('span') 
-								for n in size:
-									export_line += n.text + ';'
-								insert_input('yoox_py',export_line)
-								export_line += '/n '
-								export_file.write(export_line)
+
+# os.remove('./yoox_prod_export.log')
+logger.debug('Start models')
+all_items = get_actual_models(app_id)
+logger.debug('all_items finish')
+# получаем список продуктов из БД по текущему приложению, чтобы не было путанницы
+while True:
+	checker = []  # возможно бесполезно
+	# print(all_items['11791485OO'])
+	data = load_user_data(pagenum, session)  # загрузка новой страницы
+	#logger.debug(data)
+	try:
+		if int(next_page(data)) == pagenum:  # проверяем наличие новой страницы
+			grid = data.find('div', {'id': 'itemsGrid'})  # сетка с продуктами
+			item_in_grid = grid.findAll('div', {'class': 'col-8-24'})
+			#logger.debug(item_in_grid)
+			# поиск на всей странице в инфе о продукте
+			for item in item_in_grid:
+				# export_line = '' начинаем экспорт файл
+				prod_id = (item('div')[0]['data-current-cod10'])
+				logger.info(prod_id)
+				# код продукта, используется для поиска в словаре
+				if prod_not_none(item):
+					# если новый prod_id отсутствует в all_items, обрабатываем полностью с нуля
+					#all_items[prod_id] = item('a')[0]['href']
+					brand = item.find('div', {'class': 'brand font-bold text-uppercase'}).text
+					logger.debug(brand)
+					# export_line += prod_id + '|' + brand + '|'
+					picture = item('img')[0]['data-original']
+					if item.find('div', {'class': 'title'}):  # есть ли описание у товара
+						model = item('div', {'class': 'title'})[0].text
+					else:
+						model = prod_id
+					# export_line += description + '|'
+					logger.debug(model)
+					price_find = item.find('div', {'class': 'price'}).findAll('span')
+					# выборка цены, если со скидкой, выдаётся обе цены
+					if price_find[0]['class'][0] == 'fullprice':
+						price = price_find[0].text
+					else:
+						price = price_find[0].text + ';' + price_find[1].text
+					logger.debug(price)
+					size_find = item.find('div', {'class': 'size text-light'}).findAll('span')
+					# выборка размеров, в конце запятая
+					# insert_new_product(app_id, prod_id, brand, model, price, size_find, picture)
+					size = ''
+					for size_values in size_find:
+						size += size_values.text + ';'
+					logger.debug(size)
+					if prod_id not in all_items:
+						sql_result = insert_new_product(app_id, prod_id, brand, model, price, size, picture)
+						logger.debug(sql_result)
+						logger.info(prod_id + ' if finish')
+					else:
+						if datetime.now() > datetime.strptime(all_items.get(prod_id).get('navi_date')[:19], "%Y-%m-%d %H:%M:%S"):
+							if ''.join([c for c in price if c in '1234567890']) != all_items.get(prod_id).get('price'):
+								logger.debug('Update price')
+								sql_result = update_model_price(prod_id, app_id, price)
+							else:
+								sql_result = update_model_navidate(prod_id, app_id)
+							logger.debug(sql_result)
+							sql_result = update_model_size(prod_id, size, 1)
+							logger.debug(sql_result)
+							logger.info(prod_id + ' else finish')
 						else:
-							checker.append(n('div')[0]['data-current-cod10'])
-				else:
-					print('cya')
-					pagenum = 1
-			except Exception:
-				print(time.strftime("%Y%m%d-%H%M%S")," :ERROR: ", pagenum)
-				print(items)
-				error_export.write(time.strftime("%Y%m%d-%H%M%S"))
-				error_export.write(':ERROR:')
-				error_export.write(pagenum)
-				error_export.write('/n')
-			else:
-				print(datetime.datetime.now(), '::PASS::', pagenum)
-				pagenum += 1
-				#print(links_exp)
-				
+							logger.info(prod_id + ' is too new')
+		else:
+			logger.info('Finished, restarting')
+			pagenum = 1
+	except AttributeError as error:
+		logger.error(error.args)
+	except Exception:
+		logger.error(str(pagenum) + ' ' + str(ValueError))
+		logger.error(item)
+		# print(so_far)
+		# error_export.write(time.strftime("%Y%m%d-%H%M%S"))
+		# error_export.write(':ERROR:')
+		# error_export.write(str(pagenum))
+		# error_export.write('/n')
+	else:
+		logger.info('::PASS::' + str(pagenum))
+	finally:
+		pagenum += 1
+
